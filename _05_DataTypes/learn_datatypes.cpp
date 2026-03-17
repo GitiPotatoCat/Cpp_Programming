7f2fc172b72256662971e6f46e9dbf896a31d598
GitiPotatoCat <Soumyadip.Majumder@cognizant.com>
2026-03-17 13:46:16 +0000

    updated datatypes learning is added

--- _05_DataTypes/learn_datatypes.cpp
+++ _05_DataTypes/learn_datatypes.cpp
@@ -3,7 +3,8 @@
 using namespace std; 
 
 
-int main() {
+int main() 
+{
     /* Basic Datatypes */ 
     int var = 10; 
     cout << var << endl; 
@@ -29,18 +30,25 @@ int main() {
     
     cout << "Size of DataTypes" << endl; 
     cout << "int size: " << sizeof(int) << "bytes" << endl; 
+    cout << "int8_t: " << sizeof(int8_t) << "bytes" << endl; 
+    cout << "int16_t: " << sizeof(int16_t) << "bytes" << endl; 
+    cout << "int32_t: " << sizeof(int32_t) << "bytes" << endl; 
+    cout << "int64_t: " << sizeof(int64_t) << "bytes" << endl;
     cout << "float size: " << sizeof(float) << "bytes" << endl; 
+    cout << "_Float16: " << sizeof(_Float16) << "bytes" << endl; 
+    cout << "_Float32: " << sizeof(_Float32) << "bytes" << endl; 
+    cout << "_Float32x: " << sizeof(_Float32x) << "bytes" << endl; 
+    cout << "_Float64: " << sizeof(_Float64) << "bytes" << endl; 
+    cout << "_Float64x: " << sizeof(_Float64x) << "bytes" << endl; 
+    cout << "_float128: " << sizeof(__float128) << "bytes" << endl; 
     cout << "double size: " << sizeof(double) << "bytes" << endl; 
     cout << "bool size: " << sizeof(bool) << "bytes" << endl; 
     cout << "char size: " << sizeof(char) << "bytes" << endl; 
     cout << "string size: " << sizeof(string) << "bytes" << endl; 
     cout << "unsigned int: " << sizeof(unsigned int) << "bytes" << endl; 
     cout << "signed int: " << sizeof(signed int) << "bytes" << endl; 
-    
-    /* Derived Datatypes */ 
-
-
-    /* User-defined datatypes */ 
+    cout << "long: " << sizeof(long) << "bytes" << endl; 
+    cout << "long long: " << sizeof(long long) << "bytes" << endl; 
 
     return 0; 
 }
