b95821e1e7d5e47555a0f41ead7be7f12e7af2c7
GitiPotatoCat <Soumyadip.Majumder@cognizant.com>
2026-03-18 11:30:54 +0000

    Bitwise OR operators upto is added

--- _06_Operators/learnOperators.cpp
+++ _06_Operators/learnOperators.cpp
@@ -42,5 +42,41 @@ int main()
     printf("%d < %d -> %d\n", c, d, (c < d)); 
     printf("%d <= %d -> %d\n", c, d, (c <= d)); 
 
+
+    // Logical Operators 
+    cout << "===== Logical Operators =====" << endl;  
+
+    cout << "Logical AND (&&)" << endl; 
+    cout << " 0 AND 0 : " << (0 && 0) << endl; 
+    cout << " 0 AND 1 : " << (0 && 1) << endl; 
+    cout << " 1 AND 0 : " << (1 && 0) << endl; 
+    cout << " 1 AND 1 : " << (1 && 1) << endl; 
+
+    cout << "Logical OR (||)" << endl; 
+    cout << " 0 OR 0 : " << (0 || 0) << endl; 
+    cout << " 0 OR 1 : " << (0 || 1) << endl; 
+    cout << " 1 OR 0 : " << (1 || 0) << endl; 
+    cout << " 1 OR 1 : " << (1 || 1) << endl; 
+    
+    cout << "Logical NOT (!)" << endl; 
+    cout << "(NOT) true : " << !true << endl; 
+    cout << "(NOT) false : " << !false << endl; 
+    cout << "(NOT) 4 : " << !4 << endl;     // because 4 is a numeric value represents true so NOT operator showing opposite value, so it is false. 
+    cout << "(NOT) 0 : " << !0 << endl;  
+
+
+    // Bitwise Operators 
+    cout << "===== Bitwise Operators =====" << endl; 
+    cout << "Bitwise AND" << endl; 
+    cout << " 2 & 3 -> " << (2&3) << endl;      // 2 = 010, 3 = 011; so 2 & 3 = 010 & 011 = 010 = 2 (result). 
+    cout << " 4 & 5 -> " << (4&5) << endl;      // 4 = 100, 5 = 101; so 4 & 5 = 100 & 101 = 100 = 4 (result). 
+    cout << " 1 & 4 -> " << (1&4) << endl;      // 1 = 001, 4 = 100; so 1 & 4 = 001 & 100 = 100 = 0 (result). 
+    cout << " 1 & 5 -> " << (1&5) << endl;      // 1 = 001, 5 = 101; so 1 & 5 = 001 & 101 = 001 = 0 (result). 
+    cout << "Bitwise OR" << endl; 
+    cout << " 2 | 3 -> " << (2|3) << endl;      // 2 = 010, 3 = 011; so 2 | 3 = 010 | 011 = 011 = 3 (result). 
+    cout << " 4 | 5 -> " << (4|5) << endl;      // 4 = 100, 5 = 101; so 4 | 5 = 100 | 101 = 101 = 5 (result). 
+    cout << " 1 | 4 -> " << (1|4) << endl;      // 1 = 001, 4 = 100; so 1 | 4 = 001 | 001 = 100 = 5 (result). 
+    cout << " 1 | 5 -> " << (1|5) << endl;      // 1 = 001, 5 = 101; so 1 | 5 = 001 | 101 = 101 = 5 (result).  
+
     return 0; 
 }
