8a15ee0d4b7210fd77066a7ff6d201aa80063a64
GitiPotatoCat <Soumyadip.Majumder@cognizant.com>
2026-05-27 09:55:33 +0000

    CPP intro program is added

--- _01_Introduction/intro.cpp
+++ _01_Introduction/intro.cpp
@@ -1,9 +1,9 @@
-#include<iostream> 
-using namespace std; 
+#include <iostream>
+using namespace std;
 
 int main() 
 {
-    cout << "C++ programming" << endl; 
+    cout << "Namaste C++" << endl;
 
     return 0;
 }
